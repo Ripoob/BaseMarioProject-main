dad18a14c615a85df341c9a863eeb7939bba8d0c
Ripoob <g018887@student.staffs.ac.uk>
2023-02-16 10:29:21 +0000

    Initial commit

---
+++ MarioBaseProject/Source.cpp
@@ -0,0 +1,12 @@
+//Be grateful for humble beginnings, because the next level will always require so much more of you
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* args[])
+{
+	cout << "Awesome" << endl;
+	return 0;
+}
